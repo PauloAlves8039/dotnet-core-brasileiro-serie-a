fe3e78b0a10a4597e59e2412f03365b879577d66
PauloAlves8039 <pj38alves@gmail.com>
2020-08-05 16:46:25 -0300

    Adicionando informações sobre os clubes

--- BrasileiroSerieA/Util/Clubes.cs
+++ BrasileiroSerieA/Util/Clubes.cs
@@ -53,23 +53,111 @@ namespace BrasileiroSerieA.Util
                     case "1":
                         msgBot.SendTextMessageAsync(e.Message.Chat.Id, "Athletico Paranaense"
                                                    + Environment.NewLine + Environment.NewLine
+                                                   + "Alcunhas: Furacão, Rubro-Negro"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Fundação: 26/03/1924"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Localização: Curitiba - PR, Brasil"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Prinicpais Títulos:"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Campeonato Paranaense: 25"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Campeonato Brasileiro Série A: 1 (2001)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Campeonato Brasileiro Série B: 1 (1995)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Copa do Brasil: 1 (2019)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Copa Sul-Americana: 1 (2018)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Levain Cup/CONMEBOL: 1 (2019)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Site oficial:"
+                                                   + Environment.NewLine + Environment.NewLine
                                                    + "https://www.athletico.com.br/");
                         break;
 
                     case "2":
                         msgBot.SendTextMessageAsync(e.Message.Chat.Id, "Atlético Goianiense"
                                                    + Environment.NewLine + Environment.NewLine
+                                                   + "Alcunhas: Dragão, Dragão Campineiro"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Fundação: 02/04/1937"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Localização: Goiânia - GO, Brasil"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Prinicpais Títulos:"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Campeonato Goiano: 14"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Campeonato Brasileiro Série B: 1 (2016)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Campeonato Brasileiro Série C: 2 (1990 - 2008)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Torneio Integração Nacional: 1 (1971)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Taça dos Invictos: 2 (1956 - 1957/58)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Copa Goiás: 2 (1968 - 1998)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Site oficial:"
+                                                   + Environment.NewLine + Environment.NewLine
                                                    + "http://www.atleticogoianiense.com.br/");
                         break;
 
                     case "3":
-                        msgBot.SendTextMessageAsync(e.Message.Chat.Id, "Atlético Mineiro"
+                        msgBot.SendTextMessageAsync(e.Message.Chat.Id, "Atlético Mineiro" 
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Alcunhas: Galo, Alvinegro"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Fundação: 25/03/1908"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Localização: Belo Horizonte - BH, Brasil"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Prinicpais Títulos:"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Campeonato Mineiro: 44"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Campeonato Brasileiro Série A: 1 (1971)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Campeonato Brasileiro Série B: 1 (2006)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Copa do Brasil: 1 (2014)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Copa Libertadores da América: 1 (2013)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Copa CONMEBOL: 2 (1992 - 1997)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Recopa Sul-Americana: 1 (2015)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Site oficial:"
                                                    + Environment.NewLine + Environment.NewLine
                                                    + "https://www.atletico.com.br/");
                         break;
 
                     case "4":
                         msgBot.SendTextMessageAsync(e.Message.Chat.Id, "Bahia"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Alcunhas: Bahêa, Tricolor de Aço"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Fundação: 01/01/1931"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Localização: Salvador - BA, Brasil"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Prinicpais Títulos:"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Campeonato Baiano: 48"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Campeonato Brasileiro Série A: 2 (1959 - 1988)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Copa do Nordeste: 3 (2001 - 2002 - 2017)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Torneio dos Campeões do Nordeste: 1 (1948)"
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "* Taça Estado da Bahia 1 (2013)"                                                   
+                                                   + Environment.NewLine + Environment.NewLine
+                                                   + "Site oficial:"
                                                    + Environment.NewLine + Environment.NewLine
                                                    + "https://www.esporteclubebahia.com.br/");
                         break;
